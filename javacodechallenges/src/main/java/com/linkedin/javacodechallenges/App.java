9bc7fd65446b4a627619e0330ce86b3ad91d351a
mihaela-ciugudean <132344229+mihaela-ciugudean@users.noreply.github.com>
2023-05-08 09:17:53 +0000

    Implement isPasswordComplex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,28 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        return password.length() >= 6 
+                && password.matches(".*\\d.*")
+                && password.matches(".*[a-z].*")
+                && password.matches(".*[A-Z].*");
+
+
+      /*   boolean hasLowercaseLetter = false;
+        boolean hasUppercaseLetter = false;
+        boolean hasNumber = false;
+
+        for (int i=0; i< password.length() || !hasLowercaseLetter && !hasUppercaseLetter && !hasNumber; i++) {
+            char current = password.charAt(i);
+            if(Character.isDigit(current)) {
+                hasNumber = true;
+            } else if (Character.isUpperCase(current)) {
+                hasUppercaseLetter = true;
+            } else if (Character.isLowerCase(current)) {
+                hasLowercaseLetter = true;
+            }
+        }
+        return hasNumber && hasUppercaseLetter && hasLowercaseLetter;
+        */
     }
 
     public static void main(String[] args) {
